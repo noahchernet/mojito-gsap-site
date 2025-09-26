444c8311f8979e68c6db6923564ca5721193753f
Noah Chernet <86301024+noahchernet@users.noreply.github.com>
2025-09-26 19:40:18 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return <div></div>;
+}
+
+App.propTypes = {};
+
+export default App;
