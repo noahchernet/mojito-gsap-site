75acd62555540a5701483d013acd4f731b98b74b
Noah Chernet <86301024+noahchernet@users.noreply.github.com>
2025-09-27 11:29:29 +0300

    background video, play on scroll

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,13 @@
+import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -32,37 +37,66 @@ const Hero = () => {
           start: "top top",
           end: "bottom top",
           scrub: true,
-          markers: true,
         },
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const videoStart = isMobile ? "top 50%" : "center 60%";
+    const videoEnd = isMobile ? "120% 50%" : "bottom top";
+
+    // Video animation timeline
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: videoStart,
+        end: videoEnd,
+        scrub: true,
+        pin: true,
+        // onEnter: () => videoRef.current?.play,
+        // onEnterBack: () => videoRef.current?.play,
+        // onLeave: () => videoRef.current?.pause,
+        // onLeaveBack: () => videoRef.current?.pause,
+      },
+    });
+
+    videoRef.current!.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current!.duration,
+      });
+    };
   }, []);
   return (
-    <section id="hero" className="noisy">
-      <h1 className="title">MOJITO</h1>
+    <div>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
 
-      <img src="public/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
-      <img src="public/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
-      <div className="body">
-        <div className="content">
-          <div className="space-y-5 hidden md:block">
-            <p>Cool. Crisp. Classic</p>
-            <p className="subtitle">
-              Sip the Spirit <br /> of Summer
-            </p>
-          </div>
+        <img src="public/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
+        <img src="public/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
 
-          <div className="view-cocktails">
-            <p className="subtitle">
-              Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed
-              to delight your senses.
-            </p>
-            <a href="#cocktails">View Cocktails</a>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed
+                to delight your senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload="auto" />
       </div>
-    </section>
+    </div>
   );
 };
 
